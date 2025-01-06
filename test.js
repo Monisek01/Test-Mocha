15eaede1bb870dd3d1882ad5f41fa2724d5b29db
Monika V <monvet01@seznam.czgit config --global user.name Monika>
2025-01-07 00:48:32 +0100

    Test: Cat Facts API - import knihoven Axios a Assert

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
 // Git init - Projekt Mocha - API
+
+// Importujeme knihovnu axios pro HTTP požadavky
+const axios = require('axios');
+// Importujeme assert pro ověřování očekávaných výsledků
+const assert = require('assert');
+
