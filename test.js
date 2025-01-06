95a1d98a8ecfe1ef9942ca58e6275521bc405069
Monika V <monvet01@seznam.czgit config --global user.name Monika>
2025-01-07 00:50:55 +0100

    Test: Cats Facts API - popis testu a status, formát odpovědi

--- test.js
+++ test.js
@@ -5,3 +5,10 @@ const axios = require('axios');
 // Importujeme assert pro ověřování očekávaných výsledků
 const assert = require('assert');
 
+// Popisujeme testovací sadu "Cat Facts API"
+describe('Cat Facts API', function() {
+  // Test kontrolující odpověď z API
+  it('by měl vrátit status code 200 a odpověď ve formátu JSON', async function() {
+    try {
+      // Odesíláme GET požadavek na API
+      const response = await axios.get('https://catfact.ninja/fact');
